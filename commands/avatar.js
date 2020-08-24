e023b059983f734bc18b2dccbf7d50a7fa4de1d5
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-08-23 20:35:40 -0400

    Delete avatar.js

--- commands/avatar.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'avatar',
-	description: 'Reloads a command',
-	args: true,
-	execute(message, args) {
-        message.reply(message.author.displayAvatarURL());
-      }
-    }
