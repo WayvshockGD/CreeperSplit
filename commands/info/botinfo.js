a31de715c43a067a90ce1ac9e3854eeee929eb61
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-09-16 15:06:24 -0400

    Add files via upload

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -1,12 +1,16 @@
 const { MessageEmbed } = require('discord.js');
+const colors = require('../json/embed colors.json');
+const emote = require('../json/emotes.json')
 
 module.exports.run = (bot, message, args) => {
 
         const embed = new MessageEmbed()
        .setTitle('Bot Info:')
-       .addField('<:logosquare:745406062869610646>  discord.js version:  |', '`v12.3.1`', true)
-       .addField('<:nodejs:745402596352852152> node version:  |', '`v14.5.0`', true)
-       .addField('<:creeper:745406088102281336> bot version:', '`v1.5`', true)
+       .setColor(colors.green)
+       .addField(`${emote.discordjs} discord.js version:  |`, '`v12.3.1`', true)
+       .addField(`${emote.github} node version:  |`, '`v14.5.0`', true)
+       .addField('bot version:', '`v1.7`', true)
+       .addField('Links:', `[Github Repo](https://github.com/WayvshockGD/CreeperSplit) ${emote.github} | [support server](https://discord.gg/bN9PEde)`)
         message.channel.send(embed);
     }
 
