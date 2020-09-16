188fcdf5b47dc0304d8fbce1c328fd276f661050
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-09-16 15:51:10 -0400

    fixed a bug lol

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -7,8 +7,8 @@ module.exports.run = (bot, message, args) => {
         const embed = new MessageEmbed()
        .setTitle('Bot Info:')
        .setColor(colors.green)
-       .addField(`${emote.discordjs} discord.js version:  |`, '`v12.3.1`', true)
-       .addField(`${emote.github} node version:  |`, '`v14.5.0`', true)
+       .addField(`${emote.djs} discord.js version:  |`, '`v12.3.1`', true)
+       .addField(`${emote.node} node version:  |`, '`v14.5.0`', true)
        .addField('bot version:', '`v1.7`', true)
        .addField('Links:', `[Github Repo](https://github.com/WayvshockGD/CreeperSplit) ${emote.github} | [support server](https://discord.gg/bN9PEde)`)
         message.channel.send(embed);
