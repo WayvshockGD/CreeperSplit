c97d4a15e9a516eeb2b9fcabc1054ec20ab0364f
Irian3x3 <irian3x3@gmail.com>
2020-11-04 09:45:43 -0400

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -4,11 +4,11 @@ module.exports.run = (bot, message, args) => {
 
 		const embed = new MessageEmbed()
 		.setTitle(`Help:`)
-		.setDescription(`The prefix is: \`<\` \n \n \`<>\` are not required \n\`(disabled)\` means that command is disabled`)
+		.setDescription(`The prefix is: \`<\` \n \n \`<>\` - Not required \n\`(disabled)\` - Command has been disabled`)
 		.addField(`:smiley: Fun:`, `\`among-us (disabled), clyde, dankmeme (disabled), meme (disabled), pp, sacrifice \``, false)
 		.addField(`:exclamation: info:`, `\`botinfo, invite, changelog, serverinfo, ping, privacy\``)
 		.addField(`:shield: Moderation:`, `\`Ban <mention> <reason>, Kick <mention> <reason>, slowmode <number>\``)
-		.addField(`:regional_indicator_e: Other:`, `\`lenny, poll\``)
+		.addField(`:regional_indicator_e: Other:`, `\`lenny, poll, rainbow\``)
 		.addField(`:gear: Settings:`, `\`welcome <channel>\``)
 
 		if(bot.config.owners.includes(message.author.id)) {
