a11b7c85a8ee96a72230a993ac495014c8592bac
Irian3x3 <68793401+Irian3x3@users.noreply.github.com>
2020-11-07 09:46:13 -0400

    update lenny command (no response)

--- commands/other/lenny.js
+++ commands/other/lenny.js
@@ -1,6 +1,13 @@
-module.exports.run = (bot, message, args) => {
-        
-         
+module.exports.run = async(bot, message, args) => {
+  const res = [
+    'Getting your lenny...',
+    'Lenny coming soon:tm:',
+    'Why do you want this lenny tbh'
+  ];
+  const rand = Math.floor(Math.random() * res.length);
+
+        const m = await message.channel.send(`${res[rand]}`);
+        m.edit(`( ͡° ͜ʖ ͡°)`) 
   }
   module.exports.help = {
         name: "lenny",
