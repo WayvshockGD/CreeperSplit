4c69e9dc1fd30074f7165519c22a0dd365716fcc
Irian3x3 <68793401+Irian3x3@users.noreply.github.com>
2020-11-07 08:57:23 -0400

    update help and kick - fix the errors.

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,32 +1,29 @@
-const { MessageEmbed } = require('discord.js')
+module.exports.run = async (bot, message, args) => {
 
-module.exports.run = (bot, message, args) => {
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-    let member = message.mentions.members.first()
-
-if(!message.member.hasPermission("BAN_MEMBERS")){
-    message.channel.send("You don't have the permissions to use this command!");
-}
-
-else{
-    if(!member)
+    if (!message.member.hasPermission("BAN_MEMBERS")){
+        return message.channel.send("You don't have the permissions to use this command!");
+    }
+    if(!message.guild.me.hasPermission('BAN_MEMBERS')) {
+        return message.channel.send(`I don't have Ban permissions!`)
+    }
+    if (!member)
         return message.channel.send("Please mention a valid member of this server");
     if(!member.bannable) 
-        return message.channel.send("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+        return message.channel.send("I cannot ban this user! Please make sure my role is higher than them!");
 
     let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
+    if (!reason) reason = "No reason provided";
 
-    const BanEmbed = new MessageEmbed()
-    .setDescription(`Are you sure you want to ban \`${member.user.tag}\`?`)
-    message.channel.send(BanEmbed)
-
-
-    member.ban(reason)
-        .catch(error => message.channel.send(`Sorry ${message.author} I couldn't ban the user`));
-    message.channel.send(`${member.user.tag} has been banned by ${message.author.tag} | reason: ${reason}`);
-}
+    try {
+        await member.ban(reason)
+        message.channel.send(`${member.user.tag} has been banned by ${message.author.tag} | reason: ${reason}`);
     }
+    catch(error) {
+        message.channel.send(`Sorry ${message.author} I couldn't ban the user`);
+    }
+}
 
     module.exports.help = {
         name: "ban",
